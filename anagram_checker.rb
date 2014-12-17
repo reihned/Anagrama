87a136a505e3617126e438aa9d22b92a2359eb03
reihned <reihned@gmail.com>
2014-12-17 17:02:05 -0500

    finally fixed

--- anagram_checker.rb
+++ anagram_checker.rb
@@ -13,8 +13,8 @@ end
 
 def anagram_checker_dawg(word1, word2)
 
-  word_1 = word1.gsub(/[\s^A-Za-z]/,"").downcase
-  word_2 = word2.gsub(/[\s^A-Za-z]/,"").downcase
+  word_1 = word1.gsub(/[^A-Za-z]/,"").downcase
+  word_2 = word2.gsub(/[^A-Za-z]/,"").downcase
 
   word_1_arr = word_1.split("").sort
   word_2_arr = word_2.split("").sort
